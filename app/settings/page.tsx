da4547f1ab33b5f96ce6226c41d9825657d6a5ad
VidarDev <86915969+VidarDev@users.noreply.github.com>
2024-06-26 10:23:07 +0200

    style: :lipstick: page integration

---
+++ app/settings/page.tsx
@@ -0,0 +1,83 @@
+import { Image } from '@nextui-org/image';
+import { button as buttonStyles } from '@nextui-org/theme';
+import { siteConfig } from '@/config/site';
+import { Link } from '@nextui-org/link';
+import React from 'react';
+import { Input } from '@nextui-org/input';
+
+export default function Home() {
+  return (
+    <>
+      <section
+        className={
+          'bg-theme-primary-200 relative flex min-h-[250px] flex-col rounded-b-[80px] px-4'
+        }
+      >
+        <div className={'mb-20 mt-8 flex flex-col gap-8'}>
+          <span className={'memicon-arrow px-4 text-5xl'}></span>
+          <div className={'relative w-fit'}>
+            <h1 className={'w-fit font-pangaia text-3xl font-bold leading-10'}>Paramètres</h1>
+            {/* TODO : replace */}
+          </div>
+        </div>
+      </section>
+      <section
+        className={
+          'app-translate-y-3 relative flex min-h-48 flex-col items-center justify-center pb-4'
+        }
+      >
+        <div className={'flex w-full flex-col gap-4 px-4'}>
+          <Input
+            className={'themed-input font-raleway'}
+            label="Modifier mon nom d'utilisateur"
+            type="text"
+          />
+          <Input className={'themed-input font-raleway'} label="Modifier mon email" type="email" />
+          <Input
+            className={'themed-input font-raleway'}
+            label="Modifier mon mot de passe"
+            type="password"
+          />
+        </div>
+        <div className={'mt-8 flex w-full flex-col gap-4 px-4'}>
+          <Link
+            className={`font-raleway text-sm font-normal leading-tight text-theme-neutral underline`}
+            href={'/uikit'}
+          >
+            Importer mes données
+          </Link>
+          <Link
+            className={`font-raleway text-sm font-normal leading-tight text-theme-neutral underline`}
+            href={'/uikit'}
+          >
+            Exporter mes données
+          </Link>
+        </div>
+        <div className={'mt-8 flex w-full flex-col gap-4 px-4'}>
+          <Link
+            isExternal
+            className={`${buttonStyles()} min-h-10 w-full gap-2 rounded-full border border-theme-error bg-transparent px-6 font-raleway text-sm font-bold text-theme-error`}
+            href={siteConfig.links.docs}
+          >
+            Supprimer mon compte
+            <span className={'memicon-arrow'} />{' '}
+          </Link>
+        </div>
+        <div className={'mt-8 flex w-full flex-col gap-4 px-4'}>
+          <Link
+            className={`font-raleway text-sm font-normal leading-tight text-theme-neutral underline`}
+            href={'/uikit'}
+          >
+            Conditions d’utilisation
+          </Link>
+          <Link
+            className={`font-raleway text-sm font-normal leading-tight text-theme-neutral underline`}
+            href={'/uikit'}
+          >
+            Politique de confidentialité
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+}
